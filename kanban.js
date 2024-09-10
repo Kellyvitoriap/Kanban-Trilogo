bf80138eb9d6ffcf0b6218ee59addc29721bd275
Kelly Paiva <paivavitoriakelly@gmail.com>
2024-09-10 17:50:46 -0300

    card

--- kanban.js
+++ kanban.js
@@ -1,10 +1,27 @@
 function mostrarModal() {
     var modal = document.getElementById("mymodal");
     var overlay = document.getElementById("modalOverlay");
-    modal.style.display = "block";
-    overlay.style.display = "block"
+    modal.style.display = "flex";
+    overlay.style.display = "flex";
+
+    
+
+   /* var teste = 'ABRIU O MODAL';
+
+    console.log(teste);*/
 }
-function esconderModal(){
+
+function enviarForm(){
+    var descricao = document.getElementById("desc").value;
+    var tipo= document.getElementById("type").value;
+    var responsavel = document.getElementById("resp").value;
+
+    console.log(descricao);
+    console.log(responsavel);
+    console.log(tipo);
+}
+
+function escreonderModal(){
     var modal = document.getElementById("mymodal");
     var overlay= document.getElementById("modalOverlay");
     modal.style.display = ""
